b3a4fa13e5cb17a15a5b0270dcf530b997a7429e
Ana Paula de Melo Euzébio <ana.euzebio@dynamox.net>
2024-01-05 10:26:54 -0300

    teste finalizado

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -14,13 +14,13 @@ it('Deve adicionar 4 produtos ao carrinho e ir para o checkout', () => {
     cy.get('a').contains('Ver carrinho').click()
     cy.get('a').contains('Concluir compra').click()
     cy.get('h3').contains('Detalhes de faturamento')
+    EnderecoPage.editarEnderecoFaturamento('ana', 'euzebio', 'EBAC', 'Brasil', 'Cidnei Luiz', 'random', 'Florianópolis', 'Santa Catarina', '88107492', '48988258940', 'paulaeuzebio0@gmail.com')
+    cy.get('input[id="payment_method_cod"]').click()
+    cy.get('#terms').click({ multiple: true });;
+    cy.get('input[name="woocommerce_checkout_place_order"').click();
+   
           
-});
-    it('Deve fazer checkout', () => {
-        EnderecoPage.editarEnderecoFaturamento('ana', 'euzebio')
-        cy.get('input[id="payment_method_cod"]').click()
-        cy.get('input[class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox"]').click({ multiple: true });
-        cy.get('input[name="woocommerce_checkout_place_order"').click();
-});
+    });
+
 });
 
